6fcb2fdc235336fcadb1240bd2843d409e73a0a7
umar12Fa <36240713+umar12Fa@users.noreply.github.com>
2024-11-13 04:57:07 +0500

    fix :Bump the flutter connectivity package.

--- lib/tasks/ReconnectWhenOffline.dart
+++ lib/tasks/ReconnectWhenOffline.dart
@@ -6,42 +6,51 @@ import '../domain/utils/logger.dart';
 import '../index.dart';
 import '../middleware/data/receivements/ConfigureConnectionResponseCli.dart';
 
-class ReconnectWhenOffline{
+class ReconnectWhenOffline {
   final Connectivity _connectivityManager = Connectivity();
-  StreamSubscription<ConnectivityResult>? _connectivitySubscription;
-  int secondsToDisconnectWithoutPingFromClient = ConnectionConfiguration().reconnectClientAfterMillisecondsWithoutServerPong;
-  ConnectivityResult? _connectivity;
+  StreamSubscription<List<ConnectivityResult>>? _connectivitySubscription;
+  int secondsToDisconnectWithoutPingFromClient = ConnectionConfiguration()
+      .reconnectClientAfterMillisecondsWithoutServerPong;
+  List<ConnectivityResult>? _connectivity;
   bool _isFirstTime = true;
 
-  ConnectivityResult? get connectivity => _connectivity;
+  List<ConnectivityResult>? get connectivity => _connectivity;
 
-  start(){
+  start() {
     stop();
-    Future.delayed(const Duration(seconds: 1), (){
-      _connectivitySubscription =
-          _connectivityManager.onConnectivityChanged.listen((ConnectivityResult conn) {
-            if(_isFirstTime){
-              _isFirstTime = false;
-              return;
-            }
-            _connectivity = conn;
+    Future.delayed(const Duration(seconds: 1), () {
+      _connectivitySubscription = _connectivityManager.onConnectivityChanged
+          .listen((List<ConnectivityResult> conn) {
+        if (_isFirstTime) {
+          _isFirstTime = false;
+          return;
+        }
+        _connectivity = conn;
 
-            if (conn == ConnectivityResult.none) {
-              getIt.get<ConnectionService>().notifyConnectionChanged(ConnectionStatus.disconnected);
-              logger('Lost internet connection',);
-            } else {
-              logger('Got internet connection, reconnecting...', level: Level.debug);
-              try {
-                getIt.get<ConnectionService>().reconnect();
-              } catch(e) {
-                 logger('ReconnectWhenOffline', level:  Level.error, additionalData: e);
-              }
-            }
-          });
+        if (!(conn.contains(ConnectivityResult.mobile) ||
+            conn.contains(ConnectivityResult.wifi) ||
+            conn.contains(ConnectivityResult.ethernet))) {
+          getIt
+              .get<ConnectionService>()
+              .notifyConnectionChanged(ConnectionStatus.disconnected);
+          logger(
+            'Lost internet connection',
+          );
+        } else {
+          logger('Got internet connection, reconnecting...',
+              level: Level.debug);
+          try {
+            getIt.get<ConnectionService>().reconnect();
+          } catch (e) {
+            logger('ReconnectWhenOffline',
+                level: Level.error, additionalData: e);
+          }
+        }
+      });
     });
   }
 
-  stop(){
+  stop() {
     _connectivitySubscription?.cancel();
   }
 }
